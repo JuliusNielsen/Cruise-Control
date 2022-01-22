ef38bf8ad56602d3dcf4371a660028d1a13bc2ee
JuliusDK1 <88944482+JuliusDK1@users.noreply.github.com>
2022-01-22 09:24:33 +0100

    Revert "Optimering"

--- client.lua
+++ client.lua
@@ -1,13 +1,17 @@
 local maxSpeed = 999.0
 
 RegisterCommand('cc', function(source, args, rawCommand)
-    local player = PlayerPedId()
+    local player = GetPlayerPed(-1)
     if not IsPedInAnyVehicle(player) then
         exports['mythic_notify']:DoHudText('error', "Du er ikke i noget køretøj")
     else
         if args[1] then
+            local player = GetPlayerPed(-1)
             local vehicle = GetVehiclePedIsIn(player)
             local vehicleSpeed = GetEntitySpeed(vehicle)
+            print("YES")
+            print(args[1])
+            print(vehicleSpeed)
             if args[1] + 0.0 >= vehicleSpeed then
                 maxSpeed = ((args[1] + 0.0) / 3.6) - 0.4
                 exports['mythic_notify']:DoHudText('success', "Du har sat cruise control til "..args[1].." Km/t")
@@ -24,7 +28,7 @@ end)
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(100)
-        local player = PlayerPedId()
+        local player = GetPlayerPed(-1)
         if IsPedInAnyVehicle(player) then
             local vehicle = GetVehiclePedIsIn(player)
             SetVehicleMaxSpeed(vehicle, maxSpeed)
@@ -34,18 +38,25 @@ end)
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(65)
-        if IsControlPressed(0, 20) or IsDisabledControlPressed(0, 20) then
-            local player = PlayerPedId()
+        Citizen.Wait(0)
+        if IsControlJustPressed(0, 20) or IsDisabledControlJustPressed(0, 20) then
+            local player = GetPlayerPed(-1)
             if IsPedInAnyVehicle(player) then
                 local vehicle = GetVehiclePedIsIn(player)
                 SetVehicleMaxSpeed(vehicle, 999.0)
-                
-                while IsControlPressed(0, 20) or IsDisabledControlPressed(0, 20) do
-                    Citizen.Wait(1)
-                
-                end
             end 
         end
     end
 end)
+
+--[[Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if maxSpeed < 900.0 then
+            if IsControlJustPressed(0, 31) then
+                exports['mythic_notify']:DoHudText('inform', "Ophævede cruise control")
+                maxSpeed = 999.0    
+            end
+        end
+    end
+end)]]
